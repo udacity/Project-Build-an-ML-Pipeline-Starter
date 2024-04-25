bad659018c908f7863f0ddf8f51f12fd29b7f30e
seagnome <fat.shinobi@gmail.com>
2024-04-25 06:58:12 -0500

    Add files via upload

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,6 +60,14 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_row_count(data: pd.DataFrame):
+    """
+    Check if the dataset is composed by the correct number of rows
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+    Check if the price variable is between the correct range of values
+    """
+    assert data['price'].between(min_price, max_price).all()
