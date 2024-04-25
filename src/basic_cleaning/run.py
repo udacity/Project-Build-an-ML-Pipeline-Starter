2a3c1ffba4e21bc1bb2d141bf0f668666e86ab54
seagnome <fat.shinobi@gmail.com>
2024-04-25 07:04:06 -0500

    Update run.py
    Drop rows in dataset that are outside proper geo.

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -33,7 +33,8 @@ def go(args):
     df = df[idx].copy()
     df['last_review'] = pd.to_datetime(df['last_review'])
     # TODO: add code to fix the issue happened when testing the model
-    
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     # Save the cleaned data
     logger.info('Saving and exporting cleaned data.')
