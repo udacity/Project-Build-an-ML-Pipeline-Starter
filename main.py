f3af590a472985a723f611d10d905a4f9a3eefbb
JLJ55 <111321963+JLJ55@users.noreply.github.com>
2024-11-15 00:07:02 -0500

    Update main.py
    changing basic cleaning steps

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ _steps = [
 
 
 # This automatically reads in the configuration
-@hydra.main(config_name='config', config_path='./')
+@hydra.main(config_name='config')
 def go(config: DictConfig):
 
     # Setup the wandb experiment. All runs will be grouped under this name
@@ -51,10 +51,18 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+    _ = mlflow.run(
+         os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
+         "main",
+         parameters={
+             "input_artifact": "sample.csv:latest",
+             "output_artifact": "clean_sample.csv",
+             "output_type": "clean_sample",
+             "output_description": "Data with outliers and null values removed",
+             "min_price": config['etl']['min_price'],
+             "max_price": config['etl']['max_price']
+         },
+     )
 
         if "data_check" in active_steps:
             ##################
