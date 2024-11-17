6fc17eb79927bc8ea47455b3ed1063cdf1ebb856
JLJ55 <111321963+JLJ55@users.noreply.github.com>
2024-11-17 18:36:57 -0500

    Update main.py
    updating data_split

--- main.py
+++ main.py
@@ -58,18 +58,17 @@ def go(config: DictConfig):
                 },
             )
 
-        if "data_check" in active_steps:
-            _ = mlflow.run(
-                os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
-                "main",
-                parameters={
-                    "csv": "clean_sample.csv:latest",
-                    "ref": "clean_sample.csv:reference",
-                    "kl_threshold": config["data_check"]["kl_threshold"],
-                    "min_price": config["etl"]["min_price"],
-                    "max_price": config["etl"]["max_price"],
-                },
-            )
+        if "data_split" in active_steps:
+    _ = mlflow.run(
+        f"{config['main']['components_repository']}/train_val_test_split",
+        "main",
+        parameters={
+            "input": "clean_sample.csv:latest",
+            "test_size": config["modeling"]["test_size"],
+            "random_seed": config["modeling"]["random_seed"],
+            "stratify_by": config["modeling"]["stratify_by"],
+        },
+    )
 
         if "data_split" in active_steps:
             _ = mlflow.run(
