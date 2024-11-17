635504624137b90336ea54bb5613f98ca3afe382
JLJ55 <111321963+JLJ55@users.noreply.github.com>
2024-11-17 17:11:16 -0500

    Update main.py
    fixing indention and updating train_random_forest section of code

--- main.py
+++ main.py
@@ -12,10 +12,7 @@ _steps = [
     "data_check",
     "data_split",
     "train_random_forest",
-    # NOTE: We do not include this in the steps so it is not run by mistake.
-    # You first need to promote a model export to "prod" before you can run this,
-    # then you need to run this step explicitly
-    # "test_regression_model"
+    "test_regression_model",
 ]
 
 # This automatically reads in the configuration
@@ -34,7 +31,6 @@ def go(config: DictConfig):
     with tempfile.TemporaryDirectory() as tmp_dir:
 
         if "download" in active_steps:
-            # Download file and load in W&B
             _ = mlflow.run(
                 f"{config['main']['components_repository']}/get_data",
                 "main",
@@ -58,7 +54,7 @@ def go(config: DictConfig):
                     "output_type": "clean_sample",
                     "output_description": "Data with outliers and null values removed",
                     "min_price": config['etl']['min_price'],
-                    "max_price": config['etl']['max_price']
+                    "max_price": config['etl']['max_price"]
                 },
             )
 
@@ -75,43 +71,44 @@ def go(config: DictConfig):
                 },
             )
 
-       if "data_split" in active_steps:
-    _ = mlflow.run(
-        f"{config['main']['components_repository']}/train_val_test_split",
-        "main",
-        parameters={
-            "input": "clean_sample.csv:latest",
-            "test_size": config["modeling"]["test_size"],
-            "random_seed": config["modeling"]["random_seed"],
-            "stratify_by": config["modeling"]["stratify_by"],
-            "artifact_root": "data",
-            "artifact_type": "split_data",
-        },
-    )
+        if "data_split" in active_steps:
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "artifact_root": "data",
+                    "artifact_type": "split_data",
+                },
+            )
 
         if "train_random_forest" in active_steps:
-
-            # NOTE: we need to serialize the random forest configuration into JSON
             rf_config = os.path.abspath("rf_config.json")
             with open(rf_config, "w+") as fp:
-                json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
+                json.dump(dict(config["modeling"]["random_forest"].items()), fp)
 
-            # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
-            # step
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    "output_artifact": "random_forest_export",
+                    "output_type": "model_export",
+                    "output_description": "Random Forest Model Export",
+                },
+            )
 
         if "test_regression_model" in active_steps:
-
             ##################
             # Implement here #
             ##################
-
             pass
+
 
 if __name__ == "__main__":
     go()
