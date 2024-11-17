b2cd81224c64ac91b74a396fcbf826a1599c49e1
JLJ55 <111321963+JLJ55@users.noreply.github.com>
2024-11-17 17:52:32 -0500

    Update main.py
    updating the parameters in train_random_forest

--- main.py
+++ main.py
@@ -95,6 +95,9 @@ def go(config: DictConfig):
                 "main",
                 parameters={
                     "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
                     "rf_config": rf_config,
                     "max_tfidf_features": config["modeling"]["max_tfidf_features"],
                     "output_artifact": "random_forest_export",
