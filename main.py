2e0b79ca95ce05112aabdc0aa963e60cc7b9fb8c
JLJ55 <111321963+JLJ55@users.noreply.github.com>
2024-11-17 21:11:12 -0500

    Update main.py
    changing tag name to prod

--- main.py
+++ main.py
@@ -107,7 +107,7 @@ def go(config: DictConfig):
                 os.path.join(hydra.utils.get_original_cwd(), "components", "test_regression_model"),
                 "main",
                 parameters={
-                    "mlflow_model": "random_forest_export:latest",
+                    "mlflow_model": "random_forest_export:prod",
                     "test_dataset": "test_data.csv:latest",
                 },
             )
