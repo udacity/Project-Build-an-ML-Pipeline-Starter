1d9cca5777dea7b64cd89b942ae4a3df5e3fb68c
Thais Lemes <thaislemes@Thaiss-MacBook-Pro.local>
2025-07-12 13:35:28 -0400

    Added the step  data_split

--- main.py
+++ main.py
@@ -79,10 +79,17 @@ def go(config: DictConfig):
         )
           
         if "data_split" in active_steps:
-            ##################
-            # Implement here #
-            ##################
-            pass
+            _ = mlflow.run(
+    f"{config['main']['components_repository']}/train_val_test_split",
+    'main',
+    parameters = {
+        "input":"clean_sample.csv:latest",
+        "test_size": str(config["modeling"]["test_size"]),
+        "random_seed": str(config["modeling"]["random_seed"]),
+        "stratify_by": config["modeling"]["stratify_by"],
+             },
+        )
+            
 
         if "train_random_forest" in active_steps:
 
