f79ca3e5fc4f1f34122c407112f88766ea5b60ae
Thais Lemes <thaislemes@Thaiss-MBP.attlocal.net>
2025-07-16 21:11:02 -0400

    Fixied the function train random forest

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -163,7 +163,9 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # 1 - A SimpleImputer(strategy="most_frequent") to impute missing values
     # 2 - A OneHotEncoder() step to encode the variable
     non_ordinal_categorical_preproc = make_pipeline(
-        SimpleImputer(strategy="most
+        SimpleImputer(strategy="most_frequent"),
+        OneHotEncoder()
+        
     )
     ######################################
 
