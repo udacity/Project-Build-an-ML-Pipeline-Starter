b9160c03afb5fa86fb48ae083d90ebca32a8be86
ianboyd36 <iboyd36@wgu.edu>
2025-11-30 17:40:19 -0600

    "Editing the basic_cleaning script"

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -35,7 +35,8 @@ def go(args):
     # Only implement this step when reaching Step 6: Pipeline Release and Updates
     # in the project.
     # Add longitude and latitude filter to allow test_proper_boundaries to pass
-    # ENTER CODE HERE
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
 
     # Save the cleaned data
     df.to_csv('clean_sample.csv',index=False)
