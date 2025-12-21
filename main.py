fdc3f69f0826539aaf6b0badddf2dcbd7193de85
Nage_Mclaughlin <nage.murphy@outlook.com>
2025-12-21 17:20:11 -0600

    Add test_regression_model step and evaluate prod model on test set

--- main.py
+++ main.py
@@ -136,12 +136,18 @@ def go(config: DictConfig):
 
 
         if "test_regression_model" in active_steps:
+            ######################################
+            # Implement here
+            ######################################
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest",
+                },
+            )
 
-            ##################
-            # Implement here #
-            ##################
-
-            pass
 
 
 if __name__ == "__main__":
